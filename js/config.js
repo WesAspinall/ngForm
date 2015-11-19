6b9f4ca7709dfe15569067f168f7abc1d62cda4b
Wesley Aspinall <wra427@gmail.com>
2015-11-19 18:39:16 -0500

    config file populated

---
+++ js/config.js
@@ -0,0 +1,22 @@
+let config = function($stateProvider, $urlRouterProvider) {
+
+  // A redirect, basically
+  $urlRouterProvider.otherwise('/');
+
+  // Since this is a simple app,
+  // We are omitting the template file for our root
+  $stateProvider
+    .state('root', {
+      abstract: true,
+      template: '<div class="container" ui-view></div>'
+    })
+    .state('root.home', {
+      url: '/',
+      controller: 'HomeController',
+      templateUrl: 'templates/home.tpl.html'
+    });
+};
+
+config.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+export default config;
